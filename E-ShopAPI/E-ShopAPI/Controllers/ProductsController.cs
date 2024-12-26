127fc4eef719f800bcd4ef92e65430b49af05d7c
TheForis <boriskrstovski@gmail.com>
2024-12-26 11:54:29 +0100

    added to git

---
+++ E-ShopAPI/E-ShopAPI/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using E_ShopAPI.DbAccess;
+using E_ShopAPI.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_ShopAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<ProductDto>> GetAll()
+        {
+            try
+            {
+                var productsList = StaticDb.Products.Select(x=> new ProductDto 
+                {
+                    Name = x.Name,
+                    Description = x.Description,
+                    Price = x.Price,
+                    ImageUrl = x.ImageUrl
+                }).ToList();
+                return Ok(productsList);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Server error, please contact the administrator!");
+            }
+        }
+    }
+}
---
+++ E-ShopAPI/E-ShopAPI/DTOs/ProductCreateDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_ShopAPI.DTOs
+{
+    public class ProductCreateDto
+    {
+
+        [Required(ErrorMessage = "Name cannot be empty!")]
+        [MinLength(3, ErrorMessage = "Name must contain at least 3 characters!")]
+        public string Name { get; set; }
+        [MaxLength(250)]
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+        public string ImageUrl { get; set; }
+        public int CategoryId { get; set; }
+    }
+
+}
